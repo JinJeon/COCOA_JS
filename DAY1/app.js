4b922b4b4bc52f18248c3a1d239608cb307335a6
JinJeon <nanyong01@gmail.com>
2021-11-02 22:44:41 +0900

    11/02

--- DAY1/app.js
+++ DAY1/app.js
@@ -1,72 +1,99 @@
-// 추후 계산 순서, 값 출력 위해 빈 배열 생성
-let returnArr = [];
-let figureArr = [];
+let sequenceArr = [];
 
-// 하나의 함수로 설정
-const getArea = (figure, a, b, c) => {
-  // 여러 개의 원 넓이 합 구하기. Boolean으로 b의 존재 유무를 확인
-  if (figure === "circle" && Boolean(b) === true) {
-    let circleArr = [];
-    for (let i = a; i <= b; i++) {
-      circleArr.push(i * Math.PI);
+const CAUTION = "NOT ALLOWED INPUT";
+const CIRCLE = "circle";
+const RECT = "rect";
+const TRAPE = "trapezoid";
+const figureArr = [CIRCLE, RECT, TRAPE];
+
+// 적정 값 확인 (문제 시 true)
+const checkFigure = (figure) => {
+  return figureArr.indexOf(figure) === -1;
+};
+const checkParams = (params) => {
+  return params.some((element) => {
+    if (element <= 0 || typeof element !== "number") {
+      return true;
     }
-    // 배열 내의 원의 넓이들의 합
-    const value = circleArr.reduce((A, B) => {
-      return A + B;
-    }, 0);
-    figureArr.push(figure);
-    returnArr.push(value);
-    return console.log(value);
-    // return을 사용해야 b가 없는 상황에 대해서 함수가 적용되지 않음
+  });
+};
+
+// 도형 형태 판별
+const getArea = (figure, ...value) => {
+  const params = [...value];
+  const pushSequenceArr = (figure, value) => {
+    sequenceArr.push({
+      figure,
+      value,
+    });
+    return sequenceArr;
+  };
+
+  if (checkFigure(figure)) {
+    return pushSequenceArr(CAUTION, CAUTION);
   }
-  // 원의 넓이
-  if (figure === "circle") {
-    // SUM.push(sum);
-    const value = Math.PI * a;
-    figureArr.push(figure);
-    returnArr.push(value);
-    debugger;
-    return console.log(value);
+  if (checkParams(params)) {
+    return pushSequenceArr(figure, CAUTION);
   }
-  // 직사각형의 넓이
-  if (figure === "rect") {
-    const value = a * b;
-    figureArr.push(figure);
-    returnArr.push(value);
-    return console.log(value);
+  if (figure === CIRCLE) {
+    return pushSequenceArr(CIRCLE, getCircleValue(params));
   }
-  // 사다리꼴 넓이
-  if (figure === "trapezoid") {
-    const value = ((a + b) * c) / 2;
-    figureArr.push(figure);
-    returnArr.push(value);
-    return console.log(value);
+  if (figure === RECT) {
+    return pushSequenceArr(RECT, getRectValue(params));
+  }
+  if (figure === TRAPE) {
+    return pushSequenceArr(TRAPE, getTrapeValue(params));
   }
 };
 
-// 로깅 함수 (사용한 도형, 결과 출력)
+// 도형 넓이
+const getCircleValue = (params) => {
+  if (params.length > 2 || params[0] >= params[1]) {
+    return CAUTION;
+  }
+  if (params.length === 1) {
+    return params * Math.PI;
+  }
+  if (params.length === 2) {
+    let circleArr = [];
+    for (let i = params[0]; i <= params[1]; i++) {
+      circleArr.push(i * Math.PI);
+    }
+    return circleArr.reduce((A, B) => {
+      return A + B;
+    }, 0);
+  }
+};
+const getRectValue = (params) => {
+  if (params.length !== 2) {
+    return CAUTION;
+  }
+  return params[0] * params[1];
+};
+const getTrapeValue = (params) => {
+  if (params.length !== 3) {
+    return CAUTION;
+  }
+  return ((params[0] + params[1]) * params[2]) / 2;
+};
+
+// 로깅
 const printExecutionSequence = () => {
-  let mention1 = "계산 순서 : ";
-  let mention2 = "계산 값 : ";
-  // 계산 순서 멘트 만들기
-  for (let i = 0; i < figureArr.length; i++) {
-    mention1 = mention1 + `${figureArr[i]}, `;
-  }
-  mention1 = mention1.slice(0, -2);
-  // 계산 값 멘트 만들기
-  for (let i = 0; i < returnArr.length; i++) {
-    mention2 = mention2 + `${returnArr[i]}, `;
-  }
-  mention2 = mention2.slice(0, -2);
-  // 글씨로 출력
-  console.log(mention1);
-  console.log(mention2);
+  const makeMention = (mention1, mention2) => {
+    for (let i = 0; i < sequenceArr.length; i++) {
+      mention1 = mention1 + `${sequenceArr[i].figure}, `;
+      mention2 = mention2 + `${sequenceArr[i].value}, `;
+    }
+    console.log(mention1.slice(0, -2));
+    console.log(mention2.slice(0, -2));
+  };
+  makeMention("계산 순서 : ", "계산 값 : ");
   return;
 };
 
-getArea("rect", 2, 3); // 예시 1
-getArea("circle", 5, 10); // 예시 2
-printExecutionSequence();
-getArea("circle", 1); // 예시 3
-getArea("trapezoid", 2, 3, 1); // 예시 4
+getArea(CIRCLE, 3);
+getArea(RECT, 0, 3);
+getArea(TRAPE, 2, 3, 4);
+getArea("HI", 1, 4);
+getArea(CIRCLE, 0, 4);
 printExecutionSequence();
