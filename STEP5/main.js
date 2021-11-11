6066b8b1cb76c0c7796d10623171209805fbacf5
JinJeon <nanyong01@gmail.com>
2021-11-11 12:08:15 +0900

    STEP5 main.s ERROR FIX

--- STEP5/main.js
+++ STEP5/main.js
@@ -1,12 +1,19 @@
-const data1 = "[1,2,[3,4,[5,[6]]]]";
-const data2 = "[1,2,][3,4,[5,[6]]]"; // 이 사례 해결 필요
+const data1 = "[11,2,[3,4,[5,[6]]]]";
+const data2 = "[1,2,][3,4,[5,[6]]]";
 const data3 = "[1,2,[3,4,[5,[6]]]";
+const data4 = "[1,2,[3,4,,[5,[6]]]";
 
 const run = function (data) {
-  const dataSplit = data.split("").filter((e) => e !== ",");
-  const regex = /[0-9]/g;
-  const filterArrLeft = dataSplit.filter((e) => e === "[");
-  const filterArrRight = dataSplit.filter((e) => e === "]");
+  // data 글자별로 쪼개기
+  const dataSplit = function (data) {
+    const resultArr = [];
+    data.split(",").forEach((e) => {
+      resultArr.push(...e.replace(/[0-9]/g, "").split(""));
+      resultArr.push(e.replace(/[^0-9]/g, ""));
+    });
+    return resultArr;
+  };
+  // 배열 내 숫자 찾기
   const filterNumber = function (array) {
     const resultArr = [];
     array.forEach((e) => {
@@ -14,8 +21,6 @@ const run = function (data) {
     });
     return resultArr;
   };
-  const filterNumberArr = filterNumber(dataSplit);
-
   // "]"가 "[" 보다 앞선 경우 찾기
   const isReverse = function (array) {
     let testArr = [];
@@ -24,39 +29,47 @@ const run = function (data) {
     });
     return testArr.some((val) => val > array.indexOf("]"));
   };
-  // 배열 내 객체 중 value가 존재하는 것이 있는 지 찾기
+  // (array 만들 때) 배열 내 객체 중 value가 없는 것이(type : "array") 존재하는 지
+  // => array type의 객체의 index return
   const noValueIndex = function (array) {
     const noValue = array.filter((e) => e.value === undefined);
     return array.indexOf(noValue[0]);
   };
 
+  const resultData = dataSplit(data);
+  const filterArrLeft = resultData.filter((e) => e === "[");
+  const filterArrRight = resultData.filter((e) => e === "]");
+  const filterNumberArr = filterNumber(resultData);
+
+  // error 찾기
+  if (resultData.some((e) => e === "")) return console.log('"," 갯수 오류');
   if (filterArrLeft.length !== filterArrRight.length)
-    return console.log("닫는 괄호 불일치");
-  if (isReverse(dataSplit)) return console.log("닫는 괄호가 앞섬");
+    return console.log("괄호 갯수 불일치");
+  if (isReverse(resultData)) return console.log("닫는 괄호가 앞섬");
 
   //  make Array
   let resultArr = [];
-  const makeArr = function (newObj) {
+  const makeChild = function (newObj) {
     let traverse = resultArr;
     while (noValueIndex(traverse) >= 0) {
       traverse = traverse[noValueIndex(traverse)].child;
     }
     traverse.push(newObj);
   };
-  dataSplit.forEach((e) => {
+  resultData.forEach((e) => {
     if (e === "[") {
       // type : "array"
       if (resultArr.length === 0) {
         resultArr = [{ type: "root", child: [] }];
       } else {
         let newObj = { type: "array", child: [] };
-        makeArr(newObj);
+        makeChild(newObj);
       }
     }
     if (filterNumberArr.indexOf(e) >= 0) {
       // type : "number"
       let newObj = { type: "number", value: e, child: [] };
-      makeArr(newObj);
+      makeChild(newObj);
     }
   });
 
@@ -64,13 +77,14 @@ const run = function (data) {
   console.log(
     `배열 중첩 : ${filterArrRight.length}, 원소 개수 : ${filterNumberArr.length}`
   );
-  console.log(JSON.stringify(resultArr[0])); // 확인용
-  return console.log(resultArr[0]);
+  console.log(resultArr[0]);
+  console.log(JSON.stringify(resultArr[0]));
 };
 
 run(data1);
-run(data2); // 해결하기!!!!
+run(data2);
 run(data3);
+run(data4);
 
 /* result
 [
--- STEP5/test.js
+++ STEP5/test.js
@@ -1,62 +1,12 @@
-const A = "29";
-const array = [
-  { type: "number", value: "1", child: [] },
-  { type: "array", child: [] },
-  { type: "number", value: "3", child: [] },
-  // { type: "array", child: [] },
-  { type: "number", value: "5", child: [] },
-  // { type: "array", child: [] },
-  { type: "number", value: "6", child: [] },
-];
-
-const filterNoValue = function (array) {
-  const noValue = array.filter((e) => e.value === undefined);
-  console.log(noValue);
-  return array.indexOf(noValue[1]) >= 0;
+const data1 = "[11,2,[3,43,,[5,[6]]]]";
+// split한 배열에서 ,가 반복적으로 있을 경우, ""가 나오므로 ""가 나오면 error message 출력
+const dataSplit = function (data) {
+  resultArr = [];
+  data.split(",").forEach((e) => {
+    resultArr.push(...e.replace(/[0-9]/g, "").split(""));
+    resultArr.push(e.replace(/[^0-9]/g, ""));
+  });
+  return resultArr;
 };
-// console.log(filterNoValue(array));
 
-const data = "[1,]2,[3,4,[5,[6]]]]";
-
-const dataSplit = data.split("").filter((e) => e !== ",");
-console.log(dataSplit);
-const regex = /[0-9]/g;
-const filterArrLeft = dataSplit.filter((e) => e === "[");
-const filterArrRight = dataSplit.filter((e) => e === "]");
-console.log(filterNumber);
-console.log(typeof NaN);
-console.log(dataSplit.indexOf("]"));
-let testArr = [];
-dataSplit.forEach((e, i) => {
-  if (e === "[") testArr.push(i);
-});
-console.log(testArr);
-console.log(testArr.some((val) => val > dataSplit.indexOf("]")));
-
-// result
-[
-  {
-    type: "root",
-    child: [
-      { type: "number", value: "1", child: [] },
-      { type: "number", value: "2", child: [] },
-      {
-        type: "array",
-        child: [
-          { type: "number", value: "3", child: [] },
-          { type: "number", value: "4", child: [] },
-          {
-            type: "array",
-            child: [
-              { type: "number", value: "5", child: [] },
-              {
-                type: "array",
-                child: [{ type: "number", value: "6", child: [] }],
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-];
+console.log(dataSplit(data1));
