54c2470268ad09ba83271d247b99752138d97fa6
JinJeon <nanyong01@gmail.com>
2021-11-17 17:18:46 +0900

    STEP8 app.js done

--- STEP8/app.js
+++ STEP8/app.js
@@ -1,9 +1,40 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+const deleteToDo = function (event) {
+  const targetLi = event.target.parentElement.parentElement;
+  targetLi.remove();
+};
+
+const checkToDo = function (event) {
+  const targetBox = event.target.parentElement.parentElement;
+  console.log(event);
+  console.log(targetBox);
+  targetBox.classList.toggle("delete");
+};
+
+const makeToDo = function () {
+  const toDoValue = toDoInput.value;
+  const listLi = document.createElement("li");
+  listLi.innerText = toDoValue;
+  const listSpan = document.createElement("span");
+  const checkBox = document.createElement("input");
+  checkBox.setAttribute("type", "checkbox");
+  const removeBtn = document.createElement("button");
+
+  listSpan.appendChild(checkBox);
+  listSpan.appendChild(removeBtn);
+  listLi.appendChild(listSpan);
+  toDoList.appendChild(listLi);
+
+  removeBtn.addEventListener("click", deleteToDo);
+  checkBox.addEventListener("click", checkToDo);
+};
 
 const handleToDoInput = function (event) {
   event.preventDefault();
-  console.log("HI");
+  makeToDo();
 };
 
 document.addEventListener("submit", handleToDoInput);
