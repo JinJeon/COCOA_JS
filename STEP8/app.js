2c0f934956f70b7c28a537bf7e4e81adaa6ba563
JinJeon <nanyong01@gmail.com>
2021-11-18 12:32:34 +0900

    🐶 use class

--- STEP8/app.js
+++ STEP8/app.js
@@ -1,42 +1,45 @@
 const toDoForm = document.getElementById("todo-form");
-const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
+const toDoInput = toDoForm.querySelector("input");
 
-const deleteToDo = function (event) {
-  console.log(event.target);
-  const targetLi =
-    event.target.parentElement.parentElement.parentElement.parentElement;
-  targetLi.remove();
-};
+class handleToDo {
+  deleteToDo(event) {
+    const targetLi =
+      event.target.parentElement.parentElement.parentElement.parentElement;
+    targetLi.remove();
+  }
 
-const checkToDo = function (event) {
-  const targetBox = event.target.parentElement.parentElement;
-  targetBox.classList.toggle("delete");
-};
+  checkToDo(event) {
+    const targetBox = event.target.parentElement.parentElement;
+    targetBox.classList.toggle("delete");
+  }
 
-const makeToDo = function () {
-  const toDoValue = toDoInput.value;
-  const listLi = document.createElement("li");
-  listLi.innerText = toDoValue;
-  const listSpan = document.createElement("span");
-  const checkBox = document.createElement("input");
-  checkBox.setAttribute("type", "checkbox");
-  const removeBtn = document.createElement("button");
-  removeBtn.innerHTML = '<i class="fas fa-trash"></i>';
+  makeToDo() {
+    const toDoValue = toDoInput.value;
+    const listLi = document.createElement("li");
+    listLi.innerText = toDoValue;
+    const listSpan = document.createElement("span");
+    const checkBox = document.createElement("input");
+    checkBox.setAttribute("type", "checkbox");
+    const removeBtn = document.createElement("button");
+    removeBtn.innerHTML = '<i class="fas fa-trash"></i>';
 
-  listSpan.appendChild(checkBox);
-  listSpan.appendChild(removeBtn);
-  listLi.appendChild(listSpan);
-  toDoList.appendChild(listLi);
-  toDoList.classList.add("todo_list");
+    listSpan.appendChild(checkBox);
+    listSpan.appendChild(removeBtn);
+    listLi.appendChild(listSpan);
+    toDoList.appendChild(listLi);
+    toDoList.classList.add("todo_list");
 
-  removeBtn.addEventListener("click", deleteToDo);
-  checkBox.addEventListener("click", checkToDo);
-};
+    removeBtn.addEventListener("click", this.deleteToDo);
+    checkBox.addEventListener("click", this.checkToDo);
+  }
 
-const handleToDoInput = function (event) {
-  event.preventDefault();
-  makeToDo();
-};
-
-document.addEventListener("submit", handleToDoInput);
+  handleToDoInput(event) {
+    event.preventDefault();
+    console.log(this);
+    this.makeToDo();
+  }
+}
+const newToDo = new handleToDo();
+document.addEventListener("submit", newToDo.handleToDoInput.bind(newToDo));
+console.log(this);
