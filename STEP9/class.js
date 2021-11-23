774be5d2efa163e8f8f5481ec8b3a86a746b9efd
JinJeon <nanyong01@gmail.com>
2021-11-24 00:22:45 +0900

    🔥ADD STEP9 STYLE.CSS COLOR VARIABLE

--- STEP9/class.js
+++ STEP9/class.js
@@ -1,30 +1,24 @@
-const $ = (element) => document.querySelector(`${element}`);
-class partition {
-  constructor(value) {
-    this.value = value;
-    this.valueName = document.getElementById(`${value}-name`);
-    this.valueForm = document.getElementById(`${value}-form`);
-    this.valueList = document.getElementById(`${value}-list`);
-    this.deleteIcon = document.getElementById(`${value}-delete-icon`);
-    this.valueFormInput = this.valueForm.querySelector("input");
+class ListData {
+  constructor(name) {
+    this.name = name;
+    this.titleElement = document.getElementById(`${name}-title`);
+    this.formElement = document.getElementById(`${name}-form`);
+    this.listElement = document.getElementById(`${name}-list`);
+    this.deleteIconElement = document.getElementById(`${name}-delete-icon`);
+    this.formElementInput = this.formElement.querySelector("input");
     this.listArr = [];
   }
   getArrInfo() {
-    this.listArr.push(this.valueFormInput.value);
+    this.listArr.push(this.formElementInput.value);
     localStorage.setItem(`${this.value}`, JSON.stringify(this.listArr));
   }
 }
 
-class listViewer {
-  constructor(partition) {
-    this.partition = partition;
-    this.callDeleteIcon = this.partition.deleteIcon;
-  }
-  getValuePart() {
-    const innerValue = this.partition.valueFormInput.value;
-    const valuePart = document.createElement("span");
-    valuePart.innerText = innerValue;
-    return valuePart;
+class ListViewer {
+  constructor(ListData) {
+    this.ListData = ListData;
+    this.callDeleteIcon = this.ListData.deleteIconElement;
+    this.warningElement = document.getElementById("warning");
   }
   makeIcon(figure) {
     const span = document.createElement("span");
@@ -33,24 +27,30 @@ class listViewer {
     span.appendChild(icon);
     return span;
   }
-  getCheckBox() {
+  getValuePart() {
+    const innerValue = this.ListData.formElementInput.value;
+    const valuePart = document.createElement("span");
+    valuePart.innerText = innerValue;
+    return valuePart;
+  }
+  getCheckBoxPart() {
     const checkBox = document.createElement("input");
     checkBox.setAttribute("type", "checkbox");
     return checkBox;
   }
-  getlistLi() {
+  getListPart() {
     const listLi = document.createElement("li");
     listLi.append(
       this.makeIcon("fas fa-trash"),
       this.makeIcon("far fa-edit"),
       this.getValuePart(),
-      this.getCheckBox()
+      this.getCheckBoxPart()
     );
     return listLi;
   }
   makeList() {
-    const targetLi = this.getlistLi();
-    const valueListNode = this.partition.valueList;
+    const targetLi = this.getListPart();
+    const valueListNode = this.ListData.listElement;
     valueListNode.appendChild(targetLi);
     valueListNode.classList.add("module_list");
     return targetLi;
@@ -58,11 +58,12 @@ class listViewer {
   // create list
 
   noticeWarning() {
-    $("#warning").innerText = "DOUBLE CLICK ICON TO DELETE ALL";
-    $("#warning").classList.remove("hidden");
+    const warningElement = document.getElementById("warning");
+    warningElement.innerText = "DOUBLE CLICK ICON TO DELETE ALL";
+    warningElement.classList.remove("hidden");
   }
   ifAllDelete() {
-    const valueListNode = this.partition.valueList;
+    const valueListNode = this.ListData.listElement;
     if (valueListNode.querySelector("li") === null) {
       valueListNode.classList.remove("module_list");
       this.callDeleteIcon.classList.add("hidden");
@@ -72,103 +73,105 @@ class listViewer {
     this.callDeleteIcon.classList.remove("hidden");
   }
   deleteAll() {
-    this.partition.valueList.querySelectorAll("li").forEach((e) => e.remove());
-    this.listViewer.ifAllDelete();
+    this.ListData.listElement.querySelectorAll("li").forEach((e) => e.remove());
+    this.ListViewer.ifAllDelete();
     $(
       "#warning"
-    ).innerText = `ALL DELETED (${this.partition.valueName.innerText})`;
+    ).innerText = `ALL DELETED (${this.ListData.titleElement.innerText})`;
   }
   deleteList(event) {
     const targetLi = event.target.closest("li");
     targetLi.remove();
-    this.listViewer.ifAllDelete();
+    this.ListViewer.ifAllDelete();
   }
   getEditList() {
+    const warningElement = document.getElementById("warning");
     const targetPart = event.target.closest("li").querySelectorAll("span")[2];
-    const targetValue = targetPart.innerText;
+    const targetText = targetPart.innerText;
     if (targetPart.innerHTML.length <= 28) {
-      targetPart.innerHTML = `<form><input type="text" placeholder=${targetValue} value=${targetValue} maxlength=15></form>`;
+      targetPart.innerHTML = `<form><input type="text" placeholder=${targetText} value=${targetText} maxlength=15></form>`;
     }
-    $("#warning").innerText = "PRESS ENTER TO FINISH EDITING";
-    $("#warning").classList.remove("hidden");
+    warningElement.innerText = "PRESS ENTER TO FINISH EDITING";
+    warningElement.classList.remove("hidden");
   }
   postEditList(event) {
     event.preventDefault();
     const inputValue = event.target.querySelector("input").value;
     event.target.innerHTML = `${inputValue}`;
-    $("#warning").classList.add("hidden");
+    this.warningElement.classList.add("hidden");
   }
   checkList(event) {
     const targetBox = event.target.closest("li");
     targetBox.classList.toggle("delete");
   }
   isBlank() {
-    $("#warning").classList.add("hidden");
-    if (this.partition.valueFormInput.value !== "") return false;
-    $("#warning").innerText = "FILL IN THE BLANK";
-    $("#warning").classList.remove("hidden");
+    const warningElement = document.getElementById("warning");
+    warningElement.classList.add("hidden");
+    if (this.ListData.formElementInput.value !== "") return false;
+    warningElement.innerText = "FILL IN THE BLANK";
+    warningElement.classList.remove("hidden");
     return true;
   }
 }
 // create list performance
 
-class listController {
-  constructor(partition, listViewer) {
-    this.partition = partition;
-    this.listViewer = listViewer;
-    this.callDeleteIcon = this.partition.deleteIcon;
+class ListController {
+  constructor(ListData, ListViewer) {
+    this.ListData = ListData;
+    this.ListViewer = ListViewer;
   }
   deleteAllEvent() {
-    this.listViewer.getDeleteAllIcon();
-    this.callDeleteIcon.addEventListener(
+    const deleteIconNode = this.ListData.deleteIconElement;
+    this.ListViewer.getDeleteAllIcon();
+    deleteIconNode.addEventListener(
       "click",
-      this.listViewer.noticeWarning.bind(this)
+      this.ListViewer.noticeWarning.bind(this)
     );
-    this.callDeleteIcon.addEventListener(
+    deleteIconNode.addEventListener(
       "dblclick",
-      this.listViewer.deleteAll.bind(this)
+      this.ListViewer.deleteAll.bind(this)
     );
   }
   getEditListEvent(event) {
-    this.listViewer.getEditList();
+    this.ListViewer.getEditList();
     const targetPart = event.target.closest("li").querySelectorAll("span")[2];
     targetPart.addEventListener(
       "submit",
-      this.listViewer.postEditList.bind(this)
+      this.ListViewer.postEditList.bind(this)
     );
   }
   inputListEvent(event) {
     event.preventDefault();
-    if (this.listViewer.isBlank()) return;
-    const list = this.listViewer.makeList();
+    if (this.ListViewer.isBlank()) return;
+    const list = this.ListViewer.makeList();
     const listchild = list.childNodes;
-    this.partition.getArrInfo();
-    this.partition.valueFormInput.value = "";
+    this.ListData.getArrInfo();
+    this.ListData.formElementInput.value = "";
     listchild[0].addEventListener(
       "click",
-      this.listViewer.deleteList.bind(this)
+      this.ListViewer.deleteList.bind(this)
     );
     listchild[1].addEventListener("click", this.getEditListEvent.bind(this));
     listchild[3].addEventListener(
       "click",
-      this.listViewer.checkList.bind(this)
+      this.ListViewer.checkList.bind(this)
     );
-    this.listViewer.getDeleteAllIcon();
+    this.ListViewer.getDeleteAllIcon();
     this.deleteAllEvent();
   }
   printListEvent() {
-    this.partition.valueForm.addEventListener(
+    this.ListData.formElement.addEventListener(
       "submit",
       this.inputListEvent.bind(this)
     );
   }
 }
-const todoModel = new partition("todo");
-const havingModel = new partition("having");
-const todoView = new listViewer(todoModel);
-const havingView = new listViewer(havingModel);
-const todoController = new listController(todoModel, todoView);
-const havingController = new listController(havingModel, havingView);
+const todoModel = new ListData("todo");
+const havingModel = new ListData("having");
+const todoView = new ListViewer(todoModel);
+const havingView = new ListViewer(havingModel);
+const todoController = new ListController(todoModel, todoView);
+const havingController = new ListController(havingModel, havingView);
 const init = function () {
   todoController.printListEvent();
   havingController.printListEvent();
