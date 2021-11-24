0ae4533b6bd22e37ae7d80e5146fbc7e3723ca17
JinJeon <nanyong01@gmail.com>
2021-11-24 17:19:29 +0900

    🐶STEP 11 ADD MOUSEMOVE HANDLER IN MOUSEEVENT.JS

--- STEP11/mouseEvent.js
+++ STEP11/mouseEvent.js
@@ -1,7 +1,25 @@
 const list = document.querySelector(".list");
 const listContent = document.querySelector(".list-content");
+const listChild = listContent.querySelectorAll("li");
+const result = document.querySelector(".result");
+const resultArr = [];
+
 const showContent = () => {
   listContent.classList.remove("hidden");
+};
+
+const drawResultList = () => {
+  resultLists = result.querySelector("ul");
+  if (resultLists) {
+    resultLists.remove();
+  }
+  const appendedLists = document.createElement("ul");
+  resultArr.forEach((e) => {
+    const appendedPart = document.createElement("li");
+    appendedPart.innerHTML = `${Object.keys(e)} : ${Object.values(e)}`;
+    appendedLists.appendChild(appendedPart);
+  });
+  result.appendChild(appendedLists);
 };
 
 const mouseenterHandler = () => {
@@ -11,4 +29,25 @@ const mouseenterHandler = () => {
   };
   list.addEventListener("mouseleave", mouseleaveHandler);
 };
+
+const listMousemoveHandler = (event) => {
+  const resultText = event.target.innerHTML;
+  const filterArr = resultArr.filter((e) => e[resultText] !== undefined);
+  setTimeout();
+  const countMoving = () => {
+    if (filterArr.length === 0) {
+      resultArr.push({ [resultText]: 1 });
+    } else {
+      resultArr.forEach((e) => {
+        if (typeof e[resultText] === "number") {
+          e[resultText] = e[resultText] + 1;
+        }
+      });
+    }
+    drawResultList();
+  };
+  countMoving();
+};
+
 list.addEventListener("mouseenter", mouseenterHandler);
+listChild.forEach((e) => e.addEventListener("mousemove", listMousemoveHandler));
