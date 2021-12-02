c847be63ff30fa3d5961e48e1eb2102d15bd149d
JinJeon <nanyong01@gmail.com>
2021-12-02 19:49:42 +0900

    🐶STEP13 리스트마다 각각 작동하는 타이머 기능 구현

--- STEP13/JavaScript/alarm.js
+++ STEP13/JavaScript/alarm.js
@@ -46,17 +46,25 @@ class NavigatorViewer {
     return appendedItem;
   }
 }
+
 class Viewer {
   constructor(Data) {
     this.Data = Data;
   }
+  makePadStart(number) {
+    return String(number).padStart(2, "0");
+  }
   getClock() {
     const date = new Date();
     const clock = document.querySelector(".clock");
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    const seconds = String(date.getSeconds()).padStart(2, "0");
+    const years = String(date.getFullYear());
+    const months = String(date.getMonth());
+    const days = String(date.getDay());
+    const hours = this.makePadStart(date.getHours());
+    const minutes = this.makePadStart(date.getMinutes());
+    const seconds = this.makePadStart(date.getSeconds());
     clock.innerText = `${hours}:${minutes}:${seconds}`;
+    return [years, months, days, hours, minutes, seconds];
   }
   getTimeType(event) {
     const inputValue = event.target.value;
@@ -94,6 +102,54 @@ class Viewer {
     warning.classList.remove("hidden");
     warning.innerText = mention;
   }
+  makeIcon(figure, element, link = null) {
+    const appendedItem = document.createElement(`${element}`);
+    const icon = document.createElement("i");
+    icon.setAttribute("class", `${figure}`);
+    appendedItem.appendChild(icon);
+    if (link) {
+      const anker = document.createElement("a");
+      anker.setAttribute("href", `./${link}.html`);
+      anker.appendChild(appendedItem);
+      return anker;
+    }
+    return appendedItem;
+  }
+  getValuePart(content) {
+    const valuePart = document.createElement("span");
+    valuePart.innerText = content;
+    return valuePart;
+  }
+  getCheckBoxPart() {
+    const checkBox = document.createElement("input");
+    checkBox.setAttribute("type", "checkbox");
+    return checkBox;
+  }
+  getList(contentArr) {
+    const listLi = document.createElement("li");
+    listLi.append(
+      this.getValuePart(contentArr[0]),
+      this.getValuePart(contentArr[1]),
+      this.getCheckBoxPart()
+    );
+    return listLi;
+  }
+  makeList(contentArr, item) {
+    const targetLi = this.getList(contentArr);
+    const targetNote = item.querySelector("ul");
+    targetNote.appendChild(targetLi);
+    targetNote.classList.remove("hidden");
+    return targetLi;
+  }
+  resetForm(targetForm) {
+    const targetChild = targetForm.childNodes;
+    targetChild[5].remove();
+    targetChild[1].value = "";
+  }
+  checkList(event) {
+    const targetBox = event.target.closest("li");
+    targetBox.classList.toggle("delete");
+  }
 }
 class AlarmController {
   constructor(AlarmData, Viewer, NavigatorViewer) {
@@ -116,18 +172,63 @@ class AlarmController {
     targetForm.appendChild(timeForm);
     const timeInput = timeForm.querySelector("input");
     timeInput.addEventListener("keyup", this.Viewer.getTimeType);
-    timeForm.addEventListener("submit", this.submitTimeEvent.bind(this));
+    timeForm.addEventListener("submit", (event) =>
+      this.submitTimeEvent(event, targetValue)
+    );
   }
-  submitTimeEvent(event) {
+  submitTimeEvent(event, targetValue) {
     event.preventDefault();
-    const time = event.target.querySelector("input").value;
-    const hours = time.substr(0, 2);
-    const minutes = time.substr(3, 2);
-    const seconds = time.substr(6, 2);
+    const targetItem = event.target.closest("div"); // module_item
+    const targetForm = event.target.parentNode;
+    const targetChild = targetForm.childNodes;
+
+    const targetTime = targetChild[5].querySelector("input").value;
+    const hours = targetTime.substr(0, 2);
+    const minutes = targetTime.substr(3, 2);
+    const seconds = targetTime.substr(6, 2);
+    const remainedTime = this.getRemainedTime(hours, minutes, seconds);
     if (hours > 24 || minutes > 59 || seconds > 59) {
       this.Viewer.warningMention("WRONG TIME SETTING");
       return;
     }
+    if (!remainedTime) {
+      this.Viewer.warningMention("SETTING TIME ALREADY PASSED");
+      return;
+    }
+    const contentArr = [remainedTime, targetValue];
+    const list = this.Viewer.makeList(contentArr, targetItem);
+    const listChild = list.childNodes;
+    this.Viewer.resetForm(targetForm);
+    listChild[2].addEventListener("click", this.Viewer.checkList.bind(this));
+    setInterval(() => {
+      const resetTime = this.getRemainedTime(hours, minutes, seconds);
+      listChild[0].innerHTML = resetTime;
+    }, 1000);
+  }
+  getRemainedTime(hours, minutes, seconds) {
+    const comparedTime = this.compareTime(hours, minutes, seconds);
+    if (comparedTime <= 0) return false;
+    const resultHours = this.Viewer.makePadStart(parseInt(comparedTime / 3600));
+    const resultMinutes = this.Viewer.makePadStart(
+      parseInt((comparedTime % 3600) / 60)
+    );
+    const resultSeconds = this.Viewer.makePadStart(comparedTime % 60);
+    const remainedTime = `${resultHours}:${resultMinutes}:${resultSeconds}`;
+    return remainedTime;
+  }
+  compareTime(hours, minutes, seconds) {
+    const currentTime = this.Viewer.getClock();
+    const pasttime = new Date(...currentTime);
+    const futureTime = new Date(
+      currentTime[0], // year
+      currentTime[1], // month
+      currentTime[2], // day
+      hours,
+      minutes,
+      seconds
+    );
+    const wholeSeconds = (futureTime - pasttime) / 1000;
+    return wholeSeconds;
   }
   getSubmitContentEvent() {
     const targetForm = document.querySelector(".remain_form");
@@ -138,7 +239,7 @@ class AlarmController {
   }
   operateTime() {
     this.Viewer.getClock();
-    setInterval(this.Viewer.getClock, 1000);
+    setInterval(this.Viewer.getClock.bind(this.Viewer), 1000);
   }
 }
 const navigatorViewer = new NavigatorViewer([
